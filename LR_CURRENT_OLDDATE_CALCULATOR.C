7c0823c90166b3fd6501ed6054185ae6e5926aac
wondersofplanet <165262974+wondersofplanet@users.noreply.github.com>
2024-04-27 15:09:29 +0530

    Update LR_CURRENT_OLDDATE_CALCULATOR.C
    updated code

--- LR_CURRENT_OLDDATE_CALCULATOR.C
+++ LR_CURRENT_OLDDATE_CALCULATOR.C
@@ -1,23 +1,23 @@
+Action()
+{ 
+    #include "time.h"
+    #include "stdio.h"
+    #include "lrs.h"
 
-#include "time.h"
-#include "stdio.h"
-#include "lrs.h"
-
-
-    int numberOfDays = 5;
+    int numberOfDays = 10;
     char unique_name[100]; // Assuming a maximum length for the unique name
 
     // Get current time
-    time_t currentTime = time(NULL);
+    time_t currentEpochTime = time(NULL);
     
-    // Subtract numberOfDays days (numberOfDays * 24 * 60 * 60 seconds)
-    time_t daysBefore = numberOfDays * 24 * 60 * 60;
-    time_t timeBefore = currentTime - daysBefore;
+    // Calculate time before numberOfDays days (numberOfDays * 24 * 60 * 60 seconds)
+    time_t numberOfSecondsInADay = 24 * 60 * 60;
+    time_t timeBefore = currentEpochTime - numberOfDays * numberOfSecondsInADay;
 
     // Print current time in epoch format
-    lr_output_message("Current Time (Epoch): %ld", currentTime);
+    lr_output_message("Current Time (Epoch): %ld", currentEpochTime);
     // Convert current time to human-readable format
-    lr_output_message("Current Time (Human Readable): %s", asctime(localtime(&currentTime)));
+    lr_output_message("Current Time (Human Readable): %s", asctime(localtime(&currentEpochTime)));
     
     // Print time numberOfDays before in epoch format
     lr_output_message("%d Days Before (Epoch): %ld", numberOfDays, timeBefore);
@@ -25,9 +25,10 @@
     lr_output_message("%d Days Before (Human Readable): %s", numberOfDays, asctime(localtime(&timeBefore)));
 
     // Format the unique name using epoch times
-    sprintf(unique_name, "%ld_%ld_%ld", (long)currentTime, (long)timeBefore, (long)timeBefore);
+    sprintf(unique_name, "%ld_%ld_%ld", (long)currentEpochTime, (long)timeBefore, (long)timeBefore);
     
     // Print the unique name
     lr_output_message("Unique Name: %s", unique_name);
 
-
+    return 0;
+}
