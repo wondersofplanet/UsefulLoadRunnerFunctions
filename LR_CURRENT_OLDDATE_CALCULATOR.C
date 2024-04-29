7d621ea36c362f3a8f6237af4eaf3b08f5386c59
wondersofplanet <165262974+wondersofplanet@users.noreply.github.com>
2024-04-29 11:27:42 +0530

    Update LR_CURRENT_OLDDATE_CALCULATOR.C
    --CONSIDER EPCOH TIME IN MILLISECONDS , WE ARE JUST CONACTENATING 000

--- LR_CURRENT_OLDDATE_CALCULATOR.C
+++ LR_CURRENT_OLDDATE_CALCULATOR.C
@@ -14,18 +14,18 @@ Action()
     time_t numberOfSecondsInADay = 24 * 60 * 60;
     time_t timeBefore = currentEpochTime - numberOfDays * numberOfSecondsInADay;
 
-    // Print current time in epoch format
-    lr_output_message("Current Time (Epoch): %ld", currentEpochTime);
+    // Print current time in epoch format with three zeros concatenated
+    lr_output_message("Current Time (Epoch): %ld000", currentEpochTime);
     // Convert current time to human-readable format
     lr_output_message("Current Time (Human Readable): %s", asctime(localtime(&currentEpochTime)));
     
-    // Print time numberOfDays before in epoch format
-    lr_output_message("%d Days Before (Epoch): %ld", numberOfDays, timeBefore);
+    // Print time numberOfDays before in epoch format with three zeros concatenated
+    lr_output_message("%d Days Before (Epoch): %ld000", numberOfDays, timeBefore);
     // Convert timeBefore to human-readable format
     lr_output_message("%d Days Before (Human Readable): %s", numberOfDays, asctime(localtime(&timeBefore)));
 
-    // Format the unique name using epoch times
-    sprintf(unique_name, "%ld_%ld_%ld", (long)currentEpochTime, (long)timeBefore, (long)timeBefore);
+    // Format the unique name using epoch times with three zeros concatenated
+    sprintf(unique_name, "%ld000_%ld000_%ld000", (long)currentEpochTime, (long)timeBefore, (long)timeBefore);
     
     // Print the unique name
     lr_output_message("Unique Name: %s", unique_name);
